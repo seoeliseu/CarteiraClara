24ac316343f7f8c5ced7cb112f8f299c9f1e6767
Eliseu Mendes de Morais <seoeliseu@gmail.com>
2024-08-16 08:13:41 -0300

    entidades

--- CarteiraClara.Domain/Entities/Conta.cs
+++ CarteiraClara.Domain/Entities/Conta.cs
@@ -1,28 +1,13 @@
-﻿using CarteiraClara.Core.DomainObjects;
-using CarteiraClara.Domain.Enums;
+﻿using CarteiraClara.Domain.Enums;
+using CarteiraClara.Core.DomainObjects;
 
 namespace CarteiraClara.Domain.Entities
 {
-    public class Conta : Entity, IAggregateRoot
+    public class Conta(string nome, TipoContaEnum tipoConta, string mascara, int? contaPaiId) : Entity, IAggregateRoot
     {
-        public required string Nome { get; set; }
-        public required TipoContaEnum TipoContaId { get; set; }
-        public required string Mascara { get; set; }
-        public int? ContaPaiId { get; set; }
-
-        public Conta(string nome, TipoContaEnum tipoConta, string mascara)
-        {
-            Nome = nome;
-            TipoContaId = tipoConta;
-            Mascara = mascara;
-        }
-
-        public Conta(string nome, TipoContaEnum tipoConta, string mascara, int? contaPaiId)
-        {
-            Nome = nome;
-            TipoContaId = tipoConta;
-            Mascara = mascara;
-            ContaPaiId = contaPaiId;
-        }
+        public required string Nome { get; set; } = nome;
+        public required TipoContaEnum TipoContaId { get; set; } = tipoConta;
+        public required string Mascara { get; set; } = mascara;
+        public int? ContaPaiId { get; set; } = contaPaiId;
     }
 }
---
+++ CarteiraClara.Domain/Entities/Transacao.cs
@@ -0,0 +1,12 @@
+﻿using CarteiraClara.Core.DomainObjects;
+
+namespace CarteiraClara.Domain.Entities
+{
+    public class Transacao(decimal valor, string descricao, int contaId) : Entity, IAggregateRoot
+    {
+        public required decimal Valor { get; set; } = valor;
+        public required string Descricao { get; set; } = descricao;
+        public required DateTime Data { get; set; } = DateTime.Now;
+        public required int ContaId { get; set; } = contaId;
+    }
+}
